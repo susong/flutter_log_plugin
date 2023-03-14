ac9325bbbdc303e363670b4506dfb50657bf32d5
su_song <su_song@glifemail.com>
2023-03-14 15:29:37 +0800

    更新Logger库版本为1.3.0，修复打印stack偏移的问题

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,9 +1,66 @@
+import 'dart:async';
+import 'dart:isolate';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_log_plugin/log.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 void main() {
-  runApp(const MyApp());
+  init(() {
+    runApp(const MyApp());
+  });
+}
+
+/// 初始化
+Future<void> init(VoidCallback callback) async {
+// 全局异常捕获
+  await _handleError(() async {
+    callback();
+  });
+}
+
+/// 全局异常捕获
+/// 参考 https://github.com/crazecoder/flutter_bugly/blob/master/lib/src/flutter_bugly.dart
+Future<void> _handleError(VoidCallback callback) async {
+  // 全局异常捕获
+  Isolate.current.addErrorListener(new RawReceivePort((dynamic pair) {
+    var isolateError = pair as List<dynamic>;
+    var _error = isolateError.first;
+    var _stackTrace = isolateError.last;
+    Zone.current.handleUncaughtError(_error, _stackTrace);
+  }).sendPort);
+
+  // 全局异常捕获
+  await runZonedGuarded<Future<Null>>(() async {
+    callback();
+  }, (error, stack) async {
+    Log.e('全局异常捕获', error, stack);
+  });
+
+  // 设置全局UI异常时，显示的界面
+  ErrorWidget.builder = (FlutterErrorDetails details) {
+    return const Scaffold(
+        body: Center(
+      child: Text(
+        "ERROR",
+        style: TextStyle(fontSize: 36),
+      ),
+    ));
+  };
+
+  // Flutter 提供了 FlutterError 类，
+  // 这个类的 onError 属性会在接收到框架异常时执行相应的回调
+  // 我们使用 Zone 提供的 handleUncaughtError 语句，
+  // 将 Flutter 框架的异常统一转发到当前的 Zone 中，
+  // 这样我们就可以统一使用 Zone 去处理应用内的所有异常了
+  FlutterError.onError = (FlutterErrorDetails details) {
+    if (details.stack != null) {
+      // 转发至 Zone 中
+      Zone.current.handleUncaughtError(details.exception, details.stack!);
+    } else {
+      FlutterError.presentError(details);
+    }
+  };
 }
 
 class MyApp extends StatefulWidget {
@@ -62,6 +119,16 @@ class _MyAppState extends State<MyApp> {
                     }
                   },
                   child: Text('Log.e')),
+              TextButton(
+                  onPressed: () {
+                    throw Exception('测试抛异常2');
+                  },
+                  child: Text('Log.e2')),
+              TextButton(
+                  onPressed: () {
+                    throw Exception('测试抛异常3');
+                  },
+                  child: Text('Log.e3')),
             ],
           ),
         ),
--- lib/src/log.dart
+++ lib/src/log.dart
@@ -9,9 +9,21 @@ class Log {
   static final Logger _logger = Logger(
     filter: ProductionFilter(), // release版本也打印
     printer: PrettyPrinter(
-      // 跳过方法调用栈的个数，用法看文档，分析调试源码后，传4可过滤包装的调用栈。
-      stackTraceBeginIndex: 4,
-      methodCount: 1,
+      // 跳过方法调用栈的个数，用法看文档，分析调试源码后，传1可过滤包装的调用栈。
+      stackTraceBeginIndex: 1,
+      // logger 1.3.0 版本，对methodCount实现进行了修改，真实的方法数为 methodCount-stackTraceBeginIndex
+      methodCount: 3,
+      colors: false,
+      printEmojis: false,
+      printTime: false,
+      noBoxingByDefault: true,
+    ),
+    output: LogHostOutput(),
+  );
+
+  static final Logger _logger2 = Logger(
+    filter: ProductionFilter(), // release版本也打印
+    printer: PrettyPrinter(
       colors: false,
       printEmojis: false,
       printTime: false,
@@ -21,22 +33,42 @@ class Log {
   );
 
   static void v(dynamic message, [dynamic error, StackTrace? stackTrace]) {
-    _logger.v(message, error, stackTrace);
+    if (stackTrace == null) {
+      _logger.v(message, error, stackTrace);
+    } else {
+      _logger2.v(message, error, stackTrace);
+    }
   }
 
   static void d(dynamic message, [dynamic error, StackTrace? stackTrace]) {
-    _logger.d(message, error, stackTrace);
+    if (stackTrace == null) {
+      _logger.d(message, error, stackTrace);
+    } else {
+      _logger2.d(message, error, stackTrace);
+    }
   }
 
   static void i(dynamic message, [dynamic error, StackTrace? stackTrace]) {
-    _logger.i(message, error, stackTrace);
+    if (stackTrace == null) {
+      _logger.i(message, error, stackTrace);
+    } else {
+      _logger2.i(message, error, stackTrace);
+    }
   }
 
   static void w(dynamic message, [dynamic error, StackTrace? stackTrace]) {
-    _logger.w(message, error, stackTrace);
+    if (stackTrace == null) {
+      _logger.w(message, error, stackTrace);
+    } else {
+      _logger2.w(message, error, stackTrace);
+    }
   }
 
   static void e(dynamic message, [dynamic error, StackTrace? stackTrace]) {
-    _logger.e(message, error, stackTrace);
+    if (stackTrace == null) {
+      _logger.e(message, error, stackTrace);
+    } else {
+      _logger2.e(message, error, stackTrace);
+    }
   }
 }
